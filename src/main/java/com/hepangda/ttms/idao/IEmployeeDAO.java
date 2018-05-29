6b78a272ee426e8114bdfa5c71d62f06722f79ee
hepangda <pangda@xiyoulinux.org>
2018-05-29 22:13:37 +0800

    NEW

--- src/main/java/com/hepangda/ttms/idao/IEmployeeDAO.java
+++ src/main/java/com/hepangda/ttms/idao/IEmployeeDAO.java
@@ -5,11 +5,11 @@ import com.hepangda.ttms.model.Employee;
 import java.util.ArrayList;
 
 public interface IEmployeeDAO {
-    int AddEmployee(String name,int age,int phone,String position,String password);
-    int DeleteEmployee(int ID);
-    int DeleteEmployee(String name);
+//    int AddEmployee(String name,int age,int phone,String position,String password);
+//    int DeleteEmployee(int ID);
+//    int DeleteEmployee(String name);
     int verifyLoginInfo(String username, String password);
-    ArrayList<Employee> QueryEmployee(int ID);
-    ArrayList<Employee> QueryEmployee(String name);
-
+    ArrayList<Employee> queryById(int id);
+    ArrayList<Employee> queryByName(String name);
+    ArrayList<Employee> queryByLoginName(String loginName);
 }
